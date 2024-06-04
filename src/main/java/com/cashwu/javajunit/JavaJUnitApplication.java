6cb36d09e74b95e113bce4cfc7fce28ba67f8dba
Cash <cashwugeek@icloud.com>
2024-06-04 15:23:57 +0800

    init

---
+++ src/main/java/com/cashwu/javajunit/JavaJUnitApplication.java
@@ -0,0 +1,13 @@
+package com.cashwu.javajunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJUnitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaJUnitApplication.class, args);
+    }
+
+}
